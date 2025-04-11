f079aeab5e087d6ff5579679d321afb8fd8f57ee
iKarTehFox <dperez81715+github@gmail.com>
2025-04-11 10:55:52 -0500

    chore: remove unused import again?

--- pres-mode-panel@iKarTehFox/extension.js
+++ pres-mode-panel@iKarTehFox/extension.js
@@ -3,7 +3,6 @@
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
